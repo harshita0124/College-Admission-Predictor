d0cb414ca5b3de3e79935fb7322d60b0e31d82bc
Harshita Gupta <harshita.gupta3021@gmail.com>
2025-12-14 17:37:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,12 +136,13 @@ if st.button("Predict"):
     output = model.predict(inputs)
     st.success((f"Admission Probabilty : {output[0] * 100}"))
 
-    if output < 70 and output > 50:
+    if output < 60 and output > 50:
         st.info("There are moderate chances that you get admission in the college! 🙂.")
-    elif output > 70:
+    elif output >= 60 and output < 70:
         st.info("There are high chances that you get admission in the college! 😀")
     elif output < 50:
         st.warning("There is no chances of your admission 😔!")
+
 
 
 
